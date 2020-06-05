d4bdee1699394131b841126be70887fcc906e492
Khoj Badami <khoj.badami@gmail.com>
2020-06-05 18:53:32 +0530

    Added the merger sort algoritum

---
+++ merge-sort-resources/matterJSBodyBuilders.js
@@ -0,0 +1,104 @@
+const mmToPxMupliplicationFactor = 3.78;
+const allCirclesWidthHeight = 37.510;
+
+function getColorFromSVGNode(svgNode) {
+	let styleValue = $(svgNode).attr("style");
+	let fillProperty = styleValue.split(";").find(
+		( property ) => {
+			return property.split(":")[0] == 'fill';
+		}
+	);
+	return fillProperty.split(":")[1];
+}
+
+function circleMaker(number, x, y) {
+	this.number = number;
+	this.x = x;
+	this.y = y;
+	this.makeCircle = () => {
+		let finalX = allCirclesWidthHeight / 2 + x;
+		let finalY = allCirclesWidthHeight / 2 + y;
+		return Bodies.circle(finalX, finalY, allCirclesWidthHeight / 2, {
+			render: {
+                sprite: {
+                    texture: `./merge-sort-resources/${this.number}.png`
+                }
+        	}
+		});
+	}
+}
+
+function bodyFromSvgPathMaker(svgPath, x, y, name) {
+	this.svgPath = svgPath;
+	
+	this.x = x;
+	
+	this.y = y;
+
+	this.name = name;
+	
+	this.makeBody = () => {
+		let vertexSets = [];
+		let points = Svg.pathToVertices(this.svgPath, 30);
+		vertexSets.push(Vertices.scale(points, mmToPxMupliplicationFactor, mmToPxMupliplicationFactor));
+		let body = Bodies.fromVertices(
+			0, 0,
+			vertexSets,
+			{
+                render: {
+                    fillStyle: getColorFromSVGNode(this.svgPath),
+                    strokeStyle: getColorFromSVGNode(this.svgPath),
+                    lineWidth: 1
+                },
+                isStatic : true
+            },
+            true
+		);
+
+		Matter.Body.setPosition(body, {
+			x: this.x,
+			y: this.y
+		});
+
+		let translationVector = Vector.sub(body.position, body.bounds.min);
+		Matter.Body.translate(body, translationVector);
+
+		allBodies[this.name] = body;
+		return body;
+	}
+}
+
+function bodyFromRectangle(svgRectangle, name) {
+
+	this.svgRectangle = svgRectangle;
+
+	this.name = name;
+
+	this.makeBody = () => {
+		let body = Bodies.rectangle(
+			0, 0, 
+			parseFloat($(this.svgRectangle).attr("width")) * mmToPxMupliplicationFactor, 
+			parseFloat($(this.svgRectangle).attr("height")) * mmToPxMupliplicationFactor, 
+				{
+                    render: {
+                        fillStyle: getColorFromSVGNode(this.svgRectangle),
+                        strokeStyle: getColorFromSVGNode(this.svgRectangle),
+                        lineWidth: 1
+                    },
+                    isStatic : true
+                }
+			);
+
+		Matter.Body.setPosition(body, {
+			x: parseFloat($(this.svgRectangle).attr("x")) * mmToPxMupliplicationFactor,
+			y: parseFloat($(this.svgRectangle).attr("y")) * mmToPxMupliplicationFactor
+		});
+
+		let translationVector = Vector.sub(body.position, body.bounds.min);
+		Matter.Body.translate(body, translationVector);
+
+		allBodies[this.name] = body;
+		return body;
+	}
+
+}
---
+++ merge-sort-resources/matterJSWorldSetup.js
@@ -0,0 +1,95 @@
+let Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Svg = Matter.Svg,
+    Vertices = Matter.Vertices,
+    Common = Matter.Common,
+    Vector = Matter.Vector;
+     
+let engine = Engine.create();
+ 
+let render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 1028,
+        height: 1859,
+        wireframes: false,
+        background: '#ffffff'
+    }
+});
+
+let world = engine.world;
+
+$.get( "./merge-sort-resources/static.svg", function( data ) {
+
+	// Draw All Paths
+	$(data).find('path').each(function(i, path) {
+		let fullID = $(path).attr("id");
+		let objectName = fullID.split("_")[0];
+		let objectX = fullID.split("_")[1];
+		let objectY = fullID.split("_")[2];
+		let bodyMaker = new bodyFromSvgPathMaker(
+			path, 
+			parseFloat(objectX),
+			parseFloat(objectY),
+			objectName
+		);
+		World.add(world, bodyMaker.makeBody());
+	});
+
+	// Draw All Rectangles
+	$(data).find('rect').each(function(i, svgRect) {
+		let objectName = $(svgRect).attr("id");
+		let bodyMaker = new bodyFromRectangle(
+			svgRect,
+			objectName
+		);
+		World.add(world, bodyMaker.makeBody());
+	});
+
+});
+
+let circlePostions = [
+	[378.245 + 10, 19.745],
+	[419.445 + 10, 19.745],
+	[460.645 + 10, 19.745],
+	[501.845 + 10, 19.745],
+	[543.045 + 10, 19.745],
+	[584.245 + 10, 19.745]
+];
+
+let arrayOfCircles = circlePostions.map((item) => {
+	let number = getRandomInt(0, 9);
+	numbersToBeSorted.push(number);
+	let circle = new circleMaker(
+		number, item[0], item[1]
+	).makeCircle();
+	World.add(world, circle);
+	return circle;
+});
+
+Engine.run(engine);
+Render.run(render);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+setInterval(function(){ 
+	let allStationary = true;
+	arrayOfCircles.forEach(
+		(item, idx) => {
+			if (item.speed > 0.3) {
+				allStationary = false;
+			}
+		}
+	);
+	if (allStationary && actionsWhenObjectsStopMoving[currentIndexOfActionWhenBodiesStopMoving] != undefined) {
+		actionsWhenObjectsStopMoving[currentIndexOfActionWhenBodiesStopMoving]();
+		currentIndexOfActionWhenBodiesStopMoving += 1;
+	}
+}, 1000);		
